0c4fc5667c2c7ef4b685e99637d9377bc4d486cb
DELL <DELL@DESKTOP-FB7SJMN>
2023-06-12 16:36:01 +0530

    Casting

--- Casting/src/com/casting/AddingShort.java
+++ Casting/src/com/casting/AddingShort.java
@@ -1,7 +1,7 @@
 package com.casting;
 
 public class AddingShort {
-	public static void main(String [] args) {
+	public static void main(String [] args)  {
 	short x=30;
 	short y=20;
 	short z= (short) (x+y);
